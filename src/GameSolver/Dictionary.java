ac30e2455b29124a1c687bb4461905e39ab11d6b
Pulkit Garg <pulkitgarg2014@gmail.com>
2018-06-27 19:31:57 -0500

    Workinggit add .

--- src/GameSolver/Dictionary.java
+++ src/GameSolver/Dictionary.java
@@ -22,7 +22,7 @@ public class Dictionary {
         // *********************************************************************
         String fileName = "words_alpha.txt";
         String line = null;
-        
+
         // adds all the words from the dictionary to an ArrayList
         try {
             fileReader = new FileReader(fileName);
@@ -42,54 +42,53 @@ public class Dictionary {
         int num = 0;
         System.out.println("Please enter the number of alphabets: - ");
         num = keyboard.nextInt();
-        
+
         // takes in letters to be added by the user
         wordChecker = new String[num];
         System.out.println("Please enter the letters ");
         for (int i = 0; i < num; i++) {
             wordChecker[i] = keyboard.next();
         }
-        
-        // making a clone of the list so that our original list 
+
+        // making a clone of the list so that our original list
         // doesn't get affected
         finalWords = (ArrayList<String>) words.clone();
-        
+
         /*
-        // removes the word greater than the max. length of the word
-        for(int i = 0; i < finalWords.size(); i++) {
-            if(finalWords.get(i).length()>num) {
-                finalWords.remove(i);
-            }
-        }
-        */
-        
+         * // removes the word greater than the max. length of the word for(int
+         * i = 0; i < finalWords.size(); i++) {
+         * if(finalWords.get(i).length()>num) { finalWords.remove(i); } }
+         */
+
         ListIterator<String> it = finalWords.listIterator();
-        
+
         // removing every possible word which dosen't start with the letters
         // entered by the user and also removes the word greater than the max.
-        // length of the word 
-        
-            while(it.hasNext()) {
+        // length of the word
+
+        for (int i = 0; i < wordChecker.length; i++) {
+            while (it.hasNext()) {
                 String word = it.next();
-                if(word.length()>num) {
+                if (word.length() > num || !word.startsWith(wordChecker[i])) {
                     it.remove();
                 }
             }
-            
-            for(int i = 0; i < wordChecker.length; i++) {
-                for(int j = 0; j<finalWords.size(); j++) {
-                    if(!finalWords.get(i).startsWith(wordChecker[i])) {
-                        finalWords.remove(words.get(j));
-                    }
-                }
-            }
+        }
+
+        /*
+         * for (int i = 0; i < wordChecker.length; i++) { for (int j = 0; j <
+         * finalWords.size(); j++) { if
+         * (!finalWords.get(i).startsWith(wordChecker[i])) {
+         * finalWords.remove(words.get(j)); } } }
+         */
         
         // TESTING
-        System.out.println("Size of list: " + words.size());
-        System.out.println("Size of list after removals: " + finalWords.size());
-        for(int i = 0; i<5; i++) {
-            System.out.println("A word in the final list: " + finalWords.get(i));
-        }
         
+        System.out.println("Size of list after removals: " + finalWords.size());
+        System.out.println("Words Possible");
+        for (int i = 0; i < finalWords.size(); i++) {
+            System.out.println(finalWords.get(i));
+        }
+
     }
 }
