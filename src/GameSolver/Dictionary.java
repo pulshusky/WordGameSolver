0cdb7af70e09ba5844f5cc291e8ce83dd02c095f
Pulkit Garg <pulkitgarg2014@gmail.com>
2018-06-28 13:22:03 -0500

    Final Working Program

--- src/GameSolver/Dictionary.java
+++ src/GameSolver/Dictionary.java
@@ -5,76 +5,75 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.Scanner;
+
+/**
+ * This program asks the user for the letters and then searches through a
+ * dictionary of all the possible words from the letters passed by the user
+ * 
+ * @author Pulkit Garg
+ * @author Tye Borden
+ *
+ */
 
 public class Dictionary {
 
     private static FileReader fileReader;
     private static ArrayList<String> words = new ArrayList<String>();
     private static Scanner keyboard = new Scanner(System.in);
-    private static String[] wordChecker;
-    private static ArrayList<String> finalWords;
 
     public static void main(String[] args) {
 
-        // Adding the words to an ArrayList
-        // *********************************************************************
+        Iterator<String> intLetters;
+        String letters;
+        List<String> finalList;
         String fileName = "words_alpha.txt";
         String line = null;
 
-        // adds all the words from the dictionary to an ArrayList
         try {
             fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            // adding the words in the dictionary to an ArrayList
             while ((line = bufferedReader.readLine()) != null) {
                 words.add(line);
             }
             bufferedReader.close();
+            System.out.print("What letters do you want all combinations" + " of ? - ");
+            letters = keyboard.nextLine();
+
+            // an iterator to go over the list of dictionary words
+            intLetters = words.iterator();
+
+            // removing the words which are greater than maximum
+            // possible length of words possible from the passed
+            // in letters
+            while (intLetters.hasNext()) {
+                if (intLetters.next().length() > letters.length()) {
+                    intLetters.remove();
+                }
+            }
+
+            finalList = new ArrayList<>();
+
+            // checking in which words in the list matches with the
+            // passed in letters
+            for (int i = 0; i < words.size(); i++) {
+                if (words.get(i).matches("[" + letters + "]+")) {
+                    finalList.add(words.get(i));
+                }
+            }
+
+            // printing the final list
+            System.out.println(finalList.toString());
+
         } catch (FileNotFoundException ex) {
             System.out.println("Unable to open file '" + fileName + "'");
         } catch (IOException exc) {
             System.out.println("Error reading file '" + fileName + "'");
-        }
-        // *********************************************************************
-
-        // takes in the number of letters
-        int num = 0;
-        System.out.println("Please enter the number of alphabets: - ");
-        num = keyboard.nextInt();
-
-        // takes in letters to be added by the user
-        wordChecker = new String[num];
-        System.out.println("Please enter the letters ");
-        for (int i = 0; i < num; i++) {
-            wordChecker[i] = keyboard.next();
-        }
-
-        // making a clone of the list so that our original list
-        // doesn't get affected
-        finalWords = (ArrayList<String>) words.clone();
-
-
-        ListIterator<String> it = finalWords.listIterator();
-
-        // removing every possible word which dosen't start with the letters
-        // entered by the user and also removes the word greater than the max.
-        // length of the word
-
-        for (int i = 0; i < wordChecker.length; i++) {
-            while (it.hasNext()) {
-                String word = it.next();
-                if (word.length() > num || !word.startsWith(wordChecker[i])) {
-                    it.remove();
-                }
-            }
-        }
-
-        
-        System.out.println("Size of list after removals: " + finalWords.size());
-        System.out.println("Words Possible");
-        for (int i = 0; i < finalWords.size(); i++) {
-            System.out.println(finalWords.get(i));
         }
 
     }
